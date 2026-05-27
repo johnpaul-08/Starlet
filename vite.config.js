961bf8a8507e6bf89b7b6d4a0c98bdd7508c0cd1
Abiram <abirambijoy@gmail.com>
2026-05-27 11:44:02 +0530

    chore: add seo and performance optimizations

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
